a903d91abe497f0caee7b977f522577df0f00724
Pallava Shetty <50167989+Pallava@users.noreply.github.com>
2021-08-18 16:56:12 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/springboot/demo/apigateway/FallBackController.java
@@ -0,0 +1,21 @@
+package com.springboot.demo.apigateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+
+	
+	@GetMapping("/userServiceFallback")
+	public String userServiceFallbackMethod() {
+		return "User service is taking time to respond";
+	}
+	
+	@GetMapping("/departmentServiceFallback")
+	public String departmentServiceFallbackMethod() {
+		return "Departmennt service is taking time to respond";
+	}
+	
+	
+}
